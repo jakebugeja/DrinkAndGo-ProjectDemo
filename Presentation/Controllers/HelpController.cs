c5e4ea3e75d521ead3c058be1c54cd9f4e13e947
Jake Bugeja <Jake.Bugeja.b42775@mcast.edu.mt>
2020-12-23 18:24:33 +0100

    Add project files.

---
+++ Presentation/Controllers/HelpController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class HelpController : Controller
+    {
+        [HttpGet]
+        //This method will be called using a get request
+        //purpose is to load the page (contact.cshtml)
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]//HttpPost: Will allow method to have the same name
+                  //This method includes a method overflow
+        public IActionResult Contact(string email, string query)
+        {
+            return View();
+        }
+    }
+}
